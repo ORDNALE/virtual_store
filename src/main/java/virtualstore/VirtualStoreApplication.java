9b7756490f1ecfe2173ed87c0ec559fa4b2c08f8
ORDNALE <elandro.soares@gmail.com>
2025-06-05 22:05:46 -0400

    formatação do properties e anotaçoes exemplares na classe main

--- src/main/java/virtualstore/VirtualStoreApplication.java
+++ src/main/java/virtualstore/VirtualStoreApplication.java
@@ -3,6 +3,17 @@ package virtualstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*  
+ * Ative apenas se o Spring não detectar automaticamente:  
+ * - @EntityScan: Se entidades JPA não estiverem no pacote padrão.  
+ * - @ComponentScan: Se componentes (@Service, @Controller) não forem encontrados.  
+ * - @EnableJpaRepositories: Se repositórios JPA não forem detectados.  
+ * - @EnableTransactionManagement: Raramente necessário (já habilitado por padrão).  
+ */  
+// @EntityScan(basePackages = "virtualstore.model")  
+// @ComponentScan(basePackages = {"virtualstore.*"})  
+// @EnableJpaRepositories(basePackages = {"virtualstore.repository"})  
+// @EnableTransactionManagement  
 @SpringBootApplication
 public class VirtualStoreApplication {
 
